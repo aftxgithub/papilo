0b5121e50f720d55b663d7020ea4a4d4b6818d29
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-13 22:07:56 +0100

    :heavy_plus_sign: Define sourcer and sinker methods

--- pkg/papilo/sink.go
+++ pkg/papilo/sink.go
@@ -2,4 +2,8 @@ package papilo
 
 // Sinker defines methods for any data sink.
 // A data sink should implement storage for processed data
-type Sinker interface{}
+type Sinker interface {
+	// Sink implements storage for processed data
+	// Sink reads from the provided channel
+	Sink(in chan []byte)
+}
--- pkg/papilo/source.go
+++ pkg/papilo/source.go
@@ -1,4 +1,7 @@
 package papilo
 
 // Sourcer defines methods for a data source
-type Sourcer interface{}
+type Sourcer interface {
+	// Source implements getting data and streaming into the provided channel
+	Source(out chan []byte)
+}
