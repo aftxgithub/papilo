319d4d71b72b0659703380b181258116da12314d
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-13 22:14:00 +0100

    :heavy_plus_sign: Add a pipeline to the papilo struct

--- pkg/papilo/papilo.go
+++ pkg/papilo/papilo.go
@@ -1,7 +1,9 @@
 package papilo
 
 // Papilo is the orchestrator for a pipeline
-type Papilo struct{}
+type Papilo struct {
+	pipeline Pipeline
+}
 
 // New returns a new Papilo object
 func New() Papilo {
