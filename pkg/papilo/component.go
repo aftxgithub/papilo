86116d3390e5080463f6e2121465feea01915dd7
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-13 22:10:49 +0100

    :green_heart: Add comment for component type

--- pkg/papilo/component.go
+++ pkg/papilo/component.go
@@ -1,3 +1,6 @@
 package papilo
 
+// Component is a worker in a pipeline.
+// A component reads data from the in channel, does its work
+// and writes data to the out channel
 type Component func(in chan []byte, out chan []byte)
