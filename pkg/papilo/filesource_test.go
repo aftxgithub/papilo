0ebee3bf69e3b308de347d5b4466a3cf0c1fdacb
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-14 16:43:50 +0100

    :green_heart: Write a test for the default file source

---
+++ pkg/papilo/filesource_test.go
@@ -0,0 +1,43 @@
+package papilo
+
+import (
+	"io/ioutil"
+	"testing"
+	"time"
+)
+
+var fileSourceOutput string
+
+type TestFileSourceSink struct{}
+
+func (t TestFileSourceSink) Sink(in chan interface{}) {
+	for d := range in {
+		intmed, ok := d.([]byte)
+		if !ok {
+			panic("Expected string data in TestFileSourceSink")
+		}
+		fileSourceOutput = string(intmed)
+	}
+}
+
+func TestFileSource(t *testing.T) {
+	testFile, err := ioutil.TempFile(".", "test")
+	if err != nil {
+		t.Error(err)
+	}
+	testFileName := testFile.Name()
+	testFile.Write([]byte("Hello World!"))
+	testFile.Close()
+
+	p := New()
+	p.SetSource(NewFileSource(testFileName))
+	p.SetSink(TestFileSourceSink{})
+
+	go p.Run()
+	time.Sleep(time.Second * 2)
+	p.Stop()
+
+	if fileSourceOutput != "Hello World!" {
+		t.Errorf("Wrong data, expected 'Hello World!' got %s", fileSourceOutput)
+	}
+}
