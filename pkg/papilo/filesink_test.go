698b937ee400c29fa0cc4057c41210b2f47d17eb
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-14 17:29:32 +0100

    :green_heart: Write a test for the default file sink

---
+++ pkg/papilo/filesink_test.go
@@ -0,0 +1,42 @@
+package papilo
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+)
+
+type TestFileSinkSource struct{}
+
+func (t TestFileSinkSource) Source(out chan interface{}) {
+	out <- []byte("Hello World!")
+}
+
+func TestFileSink(t *testing.T) {
+	testFile, err := ioutil.TempFile("/tmp", "test*.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	testFileName := testFile.Name()
+	testFile.Close()
+
+	p := New()
+	p.SetSource(TestFileSinkSource{})
+	p.SetSink(NewFileSink(testFileName))
+
+	go p.Run()
+	time.Sleep(time.Second * 2)
+	p.Stop()
+
+	testFile, err = os.Open(testFileName)
+	if err != nil {
+		t.Error(err)
+	}
+	var data []byte
+	testFile.Read(data)
+
+	if string(data) != "Hello World!" {
+		t.Errorf("Wrong data, expected 'Hello World!', got %s", string(data))
+	}
+}
