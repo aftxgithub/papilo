091b55ae021c11aa314c06b34ee6dd5b0ef62030
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-14 17:04:12 +0100

    :heavy_plus_sign: Allow creating a default file source from a file descriptor

--- pkg/papilo/filesource.go
+++ pkg/papilo/filesource.go
@@ -8,6 +8,7 @@ import (
 // FileSource implements a default file data source
 type FileSource struct {
 	filepath string
+	fdesc    *os.File
 }
 
 // NewFileSource returns a new file data source for streaming lines of a file.
@@ -18,12 +19,24 @@ func NewFileSource(path string) FileSource {
 	}
 }
 
+// NewFdSource returns a new file data source for streaming lines of a file.
+// The fd parameter is an opened file to be read.
+func NewFdSource(fd *os.File) FileSource {
+	return FileSource{
+		fdesc: fd,
+	}
+}
+
 // Source is the implementation for the Sourcer interface.
 // Defined output for this source is a slice of bytes.
 func (f FileSource) Source(out chan interface{}) {
-	fd, err := os.Open(f.filepath)
-	if err != nil {
-		panic(err)
+	var fd *os.File = f.fdesc
+	if fd == nil {
+		var err error
+		fd, err = os.Open(f.filepath)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	scanner := bufio.NewScanner(fd)
