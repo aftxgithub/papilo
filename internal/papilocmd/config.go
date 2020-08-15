21817d084e05845000f55efd803632bed0c3d303
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-15 20:35:51 +0100

    :heavy_plus_sign: Update config structs

--- internal/papilocmd/config.go
+++ internal/papilocmd/config.go
@@ -7,7 +7,25 @@ import (
 var cfgName = "pipeline.yaml"
 
 // Cfg holds the pipeline configurations
-type Cfg struct{}
+type Cfg struct {
+	Pipeline pipeline `yaml:"pipeline"`
+}
+
+type pipeline struct {
+	Source     source   `yaml:"source"`
+	Components []string `yaml:"components"`
+	Sink       string   `yaml:"sink"`
+}
+
+type source struct {
+	Type   string                 `yaml:"type"`
+	Config map[string]interface{} `yaml:"config"`
+}
+
+type sink struct {
+	Type   string                 `yaml:"type"`
+	Config map[string]interface{} `yaml:"config"`
+}
 
 // Config returns a new Cfg
 func Config(filename string) *Cfg {
