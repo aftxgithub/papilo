217d040acf0544969f2dbad83512b0d5948ebb4a
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-17 19:20:17 +0100

    :green_heart: Add tests for read types

--- pkg/papilo/filesource_test.go
+++ pkg/papilo/filesource_test.go
@@ -36,7 +36,7 @@ func TestFileSource(t *testing.T) {
 
 	p := New()
 	mains := &Pipeline{
-		Sourcer: NewFileSource(testFileName, 12),
+		Sourcer: NewFileSource(testFileName, ReadTypeLine),
 		Sinker:  TestFileSourceSink{},
 	}
 	go p.Run(mains)
@@ -47,4 +47,70 @@ func TestFileSource(t *testing.T) {
 	if fileSourceOutput != "Hello World!" {
 		t.Errorf("Wrong data, expected 'Hello World!' got %s", fileSourceOutput)
 	}
+}
+
+var count int
+
+func countCmpnt(p *Pipe) {
+	var cnt int
+	for !p.IsClosed {
+		_, err := p.Next()
+		if err != nil {
+			continue
+		}
+		cnt++
+		p.Write(cnt)
+	}
+}
+
+type testFileSourceCountSink struct{}
+
+func (t testFileSourceCountSink) Sink(p *Pipe) {
+	for !p.IsClosed {
+		d, _ := p.Next()
+		data, ok := d.(int)
+		if !ok {
+			continue
+		}
+		count = data
+	}
+}
+
+func TestReadTypes(t *testing.T) {
+	testFile, err := ioutil.TempFile("/tmp", "test*.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	testFileName := testFile.Name()
+	testFile.Write([]byte("Hello World!"))
+	testFile.Close()
+
+	p := New()
+	mains := &Pipeline{
+		Sourcer:    NewFileSource(testFileName, ReadTypeLine),
+		Components: []Component{countCmpnt},
+		Sinker:     testFileSourceCountSink{},
+	}
+	go p.Run(mains)
+	time.Sleep(1 * time.Second)
+	if count != 1 {
+		t.Errorf("Expected count 1, got %d", count)
+	}
+	p.Stop()
+
+	mains.Sourcer = NewFileSource(testFileName, ReadTypeWord)
+	go p.Run(mains)
+	time.Sleep(1 * time.Second)
+	if count != 2 {
+		t.Errorf("Expected count 2, got %d", count)
+	}
+	p.Stop()
+
+	mains.Sourcer = NewFileSource(testFileName, ReadTypeByte)
+	go p.Run(mains)
+	time.Sleep(1 * time.Second)
+	if count != 12 {
+		t.Errorf("Expected count 12, got %d", count)
+	}
+	p.Stop()
 }
